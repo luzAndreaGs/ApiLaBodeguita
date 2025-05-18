45033ebd2eb79c2cc5cf3a12050e7968f94be5f6
LuzAndreaGs <luz.andrea.garcia.sena@gmail.com>
2025-05-17 20:58:08 -0600

    Activa Swagger en todos los entornos

--- Program.cs
+++ Program.cs
@@ -20,12 +20,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
